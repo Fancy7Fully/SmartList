43679d2279db70981a13a29d575196ac0823ffa2
Zhiyuan <zhou.zhiyuan.stephen@hotmail.com>
2023-10-14 16:33:15 -0400

    support check

--- SmartList/ItemCell.swift
+++ SmartList/ItemCell.swift
@@ -9,80 +9,105 @@ import Foundation
 import UIKit
 
 class ItemCell: UITableViewCell {
-  var image: UIImageView = UIImageView()
-  var titleLabel = UITextView(frame: .zero)
-  var subtitle: UILabel = UILabel()
-  var index: Int!
-  weak var controller: UITableViewController?
-  
-  var delegate: ItemCellDelegate?
-  
-  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-    super.init(style: .default, reuseIdentifier: "c")
-    setupView()
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  override func updateConstraints() {
+    var image: UIImageView = UIImageView()
+    var titleLabel = UITextView(frame: .zero)
+    var subtitle: UILabel = UILabel()
+    var indexPath: IndexPath!
+    weak var controller: UITableViewController?
     
-    NSLayoutConstraint.activate([
-      image.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-      image.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-      image.widthAnchor.constraint(equalToConstant: 24),
-      image.heightAnchor.constraint(equalToConstant: 24),
-      titleLabel.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: 8),
-      titleLabel.topAnchor.constraint(equalTo: image.topAnchor),
-      titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-      subtitle.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-      subtitle.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
-      subtitle.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-    ])
+    var delegate: ItemCellDelegate?
     
-    super.updateConstraints()
-  }
-  
-  private func setupView() {
-    titleLabel.textColor = .black
-    subtitle.textColor = .gray
-    titleLabel.textContainer.lineBreakMode = .byWordWrapping
-    titleLabel.isScrollEnabled = false
-    titleLabel.backgroundColor = .green
-    titleLabel.delegate = self
-//    titleLabel.numberOfLines = 0
-    subtitle.numberOfLines = 1
-    image.translatesAutoresizingMaskIntoConstraints = false
-    titleLabel.translatesAutoresizingMaskIntoConstraints = false
-    subtitle.translatesAutoresizingMaskIntoConstraints = false
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: "c")
+        setupView()
+    }
     
-    self.contentView.addSubview(image)
-    self.contentView.addSubview(titleLabel)
-    self.contentView.addSubview(subtitle)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
-    updateConstraints()
-  }
-  
-  func updateWithItem(item: Item, index: Int) {
-    self.index = index
-    titleLabel.text = item.name
-    subtitle.text = "subtitle is here"
-    image.image = .init(systemName: "square")
+    override func updateConstraints() {
+        
+        NSLayoutConstraint.activate([
+            image.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            image.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            image.widthAnchor.constraint(equalToConstant: 24),
+            image.heightAnchor.constraint(equalToConstant: 24),
+            titleLabel.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: 8),
+            titleLabel.topAnchor.constraint(equalTo: image.topAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            subtitle.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            subtitle.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            subtitle.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        super.updateConstraints()
+    }
     
-    self.setNeedsUpdateConstraints()
-    self.updateConstraintsIfNeeded()
-  }
+    private func setupView() {
+        titleLabel.textColor = .black
+        subtitle.textColor = .gray
+        titleLabel.textContainer.lineBreakMode = .byWordWrapping
+        titleLabel.isScrollEnabled = false
+        titleLabel.backgroundColor = .green
+        titleLabel.delegate = self
+        //    titleLabel.numberOfLines = 0
+        subtitle.numberOfLines = 1
+        image.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitle.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.contentView.addSubview(image)
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(subtitle)
+        
+        updateConstraints()
+        image.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapOnImage)))
+        image.isUserInteractionEnabled = true
+    }
+    
+    @objc private func didTapOnImage() {
+        UIView.transition(with: self.image, duration: 0.2, options: .transitionCrossDissolve) {
+            [weak self] in
+                guard let self = self else { return }
+                self.image.image = .init(systemName: "checkmark.square.fill")
+        } completion: { [weak self] _ in
+            guard let self = self else { return }
+            delegate?.didSelectChecker(self, indexPath: self.indexPath)
+        }
+    }
+    
+    func updateWithItem(item: Item, indexPath: IndexPath) {
+        self.indexPath = indexPath
+        titleLabel.text = item.name
+        subtitle.text = "subtitle is here"
+        switch item.state {
+            case .checked:
+                image.image = .init(systemName: "checkmark.square.fill")
+            case .unchecked:
+                image.image = .init(systemName: "square")
+        }
+        
+        self.setNeedsUpdateConstraints()
+        self.updateConstraintsIfNeeded()
+    }
 }
 
 extension ItemCell: UITextViewDelegate {
-  func textViewDidChange(_ textView: UITextView) {
-    self.setNeedsUpdateConstraints()
-    self.updateConstraintsIfNeeded()
-    delegate?.cellDidUpdateText(text: textView.text, index: index)
-  }
+    func textViewDidBeginEditing(_ textView: UITextView) {
+//        delegate
+        delegate?.cellWillUpdateText(self, indexPath: indexPath)
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        self.setNeedsUpdateConstraints()
+        self.updateConstraintsIfNeeded()
+        delegate?.cellDidUpdateText(self, text: textView.text, indexPath: indexPath)
+    }
 }
 
 protocol ItemCellDelegate {
-  func cellDidUpdateText(text: String, index: Int)
+    func cellDidUpdateText(_ cell: ItemCell, text: String, indexPath: IndexPath)
+    func cellWillUpdateText(_ cell: ItemCell, indexPath: IndexPath)
+    func didSelectChecker(_ cell: ItemCell, indexPath: IndexPath)
 }
--- SmartList/ViewController.swift
+++ SmartList/ViewController.swift
@@ -9,146 +9,181 @@ import UIKit
 import Combine
 
 class ViewController: UITableViewController {
-  
-  let viewModel = ViewModel()
-  var sub: Cancellable?
-  
-  enum Operation {
-    case updateAll
-    case update(rows: [IndexPath])
-    case delete(rows: [IndexPath])
-  }
-  
-  class ViewModel {
-    var items = MockModel().items
-    var data: CurrentValueSubject<Operation, Never> = CurrentValueSubject(.updateAll)
     
-    func deleteRowAt(_ index: Int) {
-      items.remove(at: index)
-      data.send(.delete(rows: [IndexPath(row: index, section: 0)]))
+    let viewModel = ViewModel()
+    var sub: Cancellable?
+    var focusSub: Cancellable?
+    
+    enum Operation {
+        case updateAll
+        case update(rows: [IndexPath])
+        case delete(rows: [IndexPath])
     }
     
-    var rowNumber: Int {
-      items.count
-    }
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    view.backgroundColor = .gray
-    tableView.dataSource = self
-    tableView.delegate = self
-    tableView.backgroundColor = .white
-    tableView.rowHeight = UITableView.automaticDimension
-    tableView.estimatedRowHeight = 500
-    
-    self.title = "a title"
-    self.tableView.register(ItemCell.self, forCellReuseIdentifier: "cell")
-//    self.navigationItem.leftBarButtonItem = self.editButtonItem
-    sub = viewModel.data.sink(receiveCompletion: { [weak self] completion in
-      guard let self = self else { return }
-      sub?.cancel()
-    }, receiveValue: { [weak self]  operation in
-      guard let self = self else { return }
-      switch operation {
-        case .update(let rows):
-          tableView.reconfigureRows(at: rows)
-        case .updateAll:
-          tableView.reloadData()
-        case .delete(let rows):
-          tableView.deleteRows(at: rows, with: .automatic)
-          tableView.reloadData()
-      }
-    })
-    
-    
-    
-
-  }
-  
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return viewModel.rowNumber
-  }
-  
-  override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-    let label = UILabel(frame: .zero)
-    label.backgroundColor = .red
-    label.textColor = .blue
-    label.text = "Title"
-    return label
-  }
-  
-  override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-    let action = UIContextualAction(style: .normal, title: "Normal") { action, view, completion in
-      completion(true)
-    }
-    let action2 = UIContextualAction(style: .destructive, title: "Destructive") { [weak self] action, view, completion in
-      guard let self = self else { return }
-      viewModel.deleteRowAt(indexPath.row)
-      completion(true)
-    }
-    let configuration = UISwipeActionsConfiguration(actions: [action, action2])
-    return configuration
-  }
-  
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    if let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? ItemCell {
-      cell.updateWithItem(item: viewModel.items[indexPath.row], index: indexPath.row)
-      cell.contentView.backgroundColor = .white
-      cell.controller = self
-      cell.delegate = viewModel
-      
-      return cell
+    class ViewModel {
+        var items = MockModel().items
+        var data: CurrentValueSubject<Operation, Never> = CurrentValueSubject(.updateAll)
+        var focusedIndexPath = PassthroughSubject<IndexPath, Never>()
+        
+        func deleteRowAt(_ index: Int) {
+            items.remove(at: index)
+            data.send(.delete(rows: [IndexPath(row: index, section: 0)]))
+        }
+        
+        var rowNumber: Int {
+            items.count
+        }
     }
     
-    let cell = ItemCell(style: .default, reuseIdentifier: "cell")
-    cell.updateWithItem(item: viewModel.items[indexPath.row], index: indexPath.row)
-    cell.contentView.backgroundColor = .white
-    cell.controller = self
-    cell.delegate = viewModel
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = .white
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 500
+        
+        self.title = "a title"
+        self.tableView.register(ItemCell.self, forCellReuseIdentifier: "cell")
+        //    self.navigationItem.leftBarButtonItem = self.editButtonItem
+        sub = viewModel.data.sink(receiveCompletion: { [weak self] completion in
+            guard let self = self else { return }
+            sub?.cancel()
+        }, receiveValue: { [weak self]  operation in
+            guard let self = self else { return }
+            switch operation {
+                case .update(let rows):
+                    tableView.reconfigureRows(at: rows)
+                case .updateAll:
+                    tableView.reloadData()
+                case .delete(let rows):
+                    tableView.deleteRows(at: rows, with: .automatic)
+                    tableView.reloadData()
+            }
+        })
+        
+        focusSub = viewModel.focusedIndexPath.sink(receiveCompletion: { [weak self] completion in
+            guard let self = self else { return }
+            focusSub?.cancel()
+        }, receiveValue: { [weak self] indexPath in
+            guard let self = self else { return }
+            self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+        })
+    }
     
-    return cell
-  }
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.rowNumber
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel(frame: .zero)
+        label.backgroundColor = .red
+        label.textColor = .blue
+        label.text = "Title"
+        return label
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .normal, title: "Normal") { action, view, completion in
+            completion(true)
+        }
+        let action2 = UIContextualAction(style: .destructive, title: "Destructive") { [weak self] action, view, completion in
+            guard let self = self else { return }
+            viewModel.deleteRowAt(indexPath.row)
+            completion(true)
+        }
+        let configuration = UISwipeActionsConfiguration(actions: [action, action2])
+        return configuration
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? ItemCell {
+            cell.updateWithItem(item: viewModel.items[indexPath.row], indexPath: indexPath)
+            cell.contentView.backgroundColor = .white
+            cell.controller = self
+            cell.delegate = viewModel
+            cell.selectionStyle = .none
+            
+            return cell
+        }
+        
+        let cell = ItemCell(style: .default, reuseIdentifier: "cell")
+        cell.updateWithItem(item: viewModel.items[indexPath.row], indexPath: indexPath)
+        cell.contentView.backgroundColor = .white
+        cell.controller = self
+        cell.delegate = viewModel
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
 }
 
 extension ViewController.ViewModel: ItemCellDelegate {
-  func cellDidUpdateText(text: String, index: Int) {
-    items[index] = Item(name: text)
-    data.send(.update(rows: [IndexPath(row: index, section: 0)]))
-  }
+    func cellDidUpdateText(_ cell: ItemCell, text: String, indexPath: IndexPath) {
+        items[indexPath.row] = Item(name: text)
+        data.send(.update(rows: [indexPath]))
+    }
+    
+    func cellWillUpdateText(_ cell: ItemCell, indexPath: IndexPath) {
+        focusedIndexPath.send(indexPath)
+    }
+    
+    func didSelectChecker(_ cell: ItemCell, indexPath: IndexPath) {
+        items[indexPath.row].state.toggle()
+        data.send(.update(rows: [indexPath]))
+    }
 }
 
 struct MockModel {
-  var items: [Item] = [
-    Item(name: "HamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburger"),
-    Item(name: "Brocolli"),
-    Item(name: "Cheese"),
-    Item(name: "Iron"),
-    Item(name: "Hamburger"),
-    Item(name: "Brocolli"),
-    Item(name: "Cheese"),
-    Item(name: "Iron"),
-    Item(name: "Hamburger"),
-    Item(name: "Brocolli"),
-    Item(name: "Cheese"),
-    Item(name: "Iron"),
-    Item(name: "Hamburger"),
-    Item(name: "Brocolli"),
-    Item(name: "Cheese"),
-    Item(name: "Iron"),
-    Item(name: "Hamburger"),
-    Item(name: "Brocolli"),
-    Item(name: "Cheese"),
-    Item(name: "Iron"),
-    Item(name: "Hamburger"),
-    Item(name: "Brocolli"),
-    Item(name: "Cheese"),
-    Item(name: "Iron")
-  ]
+    var items: [Item] = [
+        Item(name: "HamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburgerHamburger"),
+        Item(name: "Brocolli"),
+        Item(name: "Cheese"),
+        Item(name: "Iron"),
+        Item(name: "Hamburger"),
+        Item(name: "Brocolli"),
+        Item(name: "Cheese"),
+        Item(name: "Iron"),
+        Item(name: "Hamburger"),
+        Item(name: "Brocolli"),
+        Item(name: "Cheese"),
+        Item(name: "Iron"),
+        Item(name: "Hamburger"),
+        Item(name: "Brocolli"),
+        Item(name: "Cheese"),
+        Item(name: "Iron"),
+        Item(name: "Hamburger"),
+        Item(name: "Brocolli"),
+        Item(name: "Cheese"),
+        Item(name: "Iron"),
+        Item(name: "Hamburger"),
+        Item(name: "Brocolli"),
+        Item(name: "Cheese"),
+        Item(name: "Iron")
+    ]
 }
 
 struct Item {
-  var name: String
+    enum State {
+        case unchecked
+        case checked
+        
+        mutating func toggle() {
+            switch self {
+                case .unchecked:
+                    self = .checked
+                case .checked:
+                    self = .unchecked
+            }
+        }
+    }
+    var name: String
+    var state: State = .unchecked
 }
